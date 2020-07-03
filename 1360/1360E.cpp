2f42f221cfdf372a80903e6c8d2dfd0ec3823ab8
r9729104234 <r9729104234@gmail.com>
2020-07-03 13:19:19 +0530

    Add 1360A/B/C/D/E/F

---
+++ 1360/1360E.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+#include<ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+#define int long long
+#define ld long double
+#define pb push_back
+#define pa pair<int,int>
+#define v1d vector<int>
+#define v2d vector<vector<int>>
+#define v1p vector<pa> 
+#define INF LLONG_MAX
+#define M 998244353 
+#define mp make_pair
+#define RS(v)    sort(v.rbegin(),v.rend())
+#define PI 3.14159265
+#define ff first
+#define ss second
+#define endl "\n"
+#define v1s vector<string>
+#define S(v) sort(v.begin(),v.end())
+#define mxpq priority_queue <int>
+#define mnpq priority_queue<int,vector<int>,greater<int>>
+#define flush cout.flush();
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> multipbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> pbds;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void dfs(int i, int j, vector<string> &str){
+
+    if(i < 0 || j < 0 || str[i][j] != '1'){
+        return;
+    }
+
+    str[i][j] = '3';
+
+    dfs(i-1, j , str);
+    dfs(i, j-1 , str);
+
+}
+
+void solve(){
+    
+    int n;
+    cin>>n;
+
+    vector<string> str(n);
+
+    for(int i = 0 ; i < n  ;i++){
+        cin>>str[i];
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        if(str[i][n-1] == '1'){
+            dfs(i, n-1, str);
+        }
+
+        if(str[n-1][i] == '1'){
+            dfs(n-1, i , str);
+        }
+    }
+
+    for(auto i : str){
+        for(auto j : i){
+            if(j == '1'){
+                cout<<"NO\n";
+                return;
+            }
+        }
+    }
+
+    cout<<"YES\n";
+
+    
+}    
+
+
+ 
+int32_t main(){
+    
+    cout<<fixed<<setprecision(16);
+    cin.sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+ 
+    // #ifndef ONLINE_JUDGE
+    //  freopen("input.txt",  "r",  stdin);
+    //  freopen("output.txt", "w", stdout);
+    // #endif
+    
+    clock_t z = clock();
+    int tc = 1;
+    int t = 0;
+    cin>>tc;
+    
+    while(tc--){
+        solve();
+    }
+
+
+ 
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << '\n';
+
+ 
+ 
+}  
+
+
